498ec92fdb766e0a7471387de4c946c5e7c65f54
jamalesenkanova <esenkanova@gmail.com>
2022-07-31 21:54:50 +0300

    jamal's update

--- src/featureGroup2/SoundCheck.java
+++ src/featureGroup2/SoundCheck.java
@@ -3,5 +3,6 @@ package featureGroup2;
 public class SoundCheck {
     public static void main(String[] args) {
         System.out.println("Hello Group2, sound check!");
+        System.out.println("Hi back from Jamal");
     }
 }
