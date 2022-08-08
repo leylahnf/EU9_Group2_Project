74ea846694ac4a4e8b11ea7a0a07b83803f37840
Sukru <sukruozen@gmail.com>
2022-08-09 01:39:19 +0300

    parent classes created

---
+++ src/scrumTask/Tester.java
@@ -0,0 +1,7 @@
+package scrumTask;
+
+public class Tester extends Employee {
+    public Tester(final String name, final char gender, final int age, final boolean isFullTime, final String jobTitle, final int employeeId, final double salary, final int experienceInYears) {
+        super(name, gender, age, isFullTime, jobTitle, employeeId, salary, experienceInYears);
+    }
+}
