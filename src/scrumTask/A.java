249ecc893697faeb85c3e630ef15d0fa6edf22dd
hsnakd <hsnakd10@gmail.com>
2022-08-09 01:24:50 +0300

    A

--- src/scrumTask/A.java
+++ src/scrumTask/A.java
@@ -1,4 +1,59 @@
 package scrumTask;
 
 public class A {
+    private String name;
+    private char gender;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Person(String name, char gender, int age) {
+        setName(name);
+        setGender(gender);
+        setAge(age);
+    }
+
+    public void eat (){
+        System.out.println(name + " is eating");
+    }
+
+    public void drink (){
+        System.out.println(name + " is drinking");
+    }
+
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", gender=" + gender +
+                ", age=" + age +
+                '}';
+    }
 }
+
+/*
+A) Class Person
+    name, gender,age
+    eat(), drink()
+ */}
